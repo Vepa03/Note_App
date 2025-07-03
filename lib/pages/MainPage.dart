3983acb213485afc99b6c9fbe37b0aad195c0c7d
Vepa Babayev <vepababayev03@gmail.com>
2025-07-03 15:32:42 +0200

    adde

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -7,6 +7,37 @@ class Mainpage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("NoteApp"),),
+      body: Center(
+        child: Text("Text"),
+      ),
+      floatingActionButton: 
+        FloatingActionButton(onPressed: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> AddNote()));
+        }, child: Icon(Icons.add),),
+    );
+  }
+}
+
+
+
+class AddNote extends StatelessWidget {
+  const AddNote({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("AddNote"),),
+      body: Column(
+        children: [
+          TextFormField(
+            style: TextStyle(fontSize: 28, color: Colors.black),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "dwed",
+            ),
+          )
+        ],
+      )
     );
   }
 }
